843bbfc6d197376dd31d742fcdbb106260cc259f
letyresina <leticiaresina@gmail.com>
2023-03-24 11:02:12 -0300

    Colocando identificação no início do código

---
+++ atividade.py
@@ -0,0 +1,11 @@
+"""
+    Checkpoint 1 - 23/03/2023 
+    Desenvolvido para as aulas de Computational Thinking - 1ESPH
+    Alunas: 
+        Camila Padalino - RM: 98316
+        Leticia Resina - RM: 98069
+ """
+
+#Código aqui
+
+
